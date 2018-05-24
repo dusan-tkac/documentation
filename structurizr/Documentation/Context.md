55a7dd6daaa8536ac47f2d63743785c322a9d7d6
dusan.tkac <dusan.tkac@gmail.com>
2018-05-24 16:13:24 +0800

    Created enterprise context view and started working on the documentation Also generating views as an input for PlantUML.

---
+++ structurizr/Documentation/Context.md
@@ -0,0 +1,58 @@
+# Context
+
+## Enterprise context
+
+This document describes some of the software systems used by IPRO Supply Chain Planning (SCP).
+
+*Note: The "Software System" and other terms used in this document are terms adopted from the [C4 model](https://c4model.com/)*
+
+Following diagram provides an overview of majority of related software systems used by SCP:
+
+![Enterprise context diagram](embed:EnterpriseContext)
+
+The SCP "is in the business" of answering question (most often) related to supply chain by creating and running models and analyzing and interpreting results of these model runs.
+
+Software systems displayed in the diagram above support users in these tasks.
+
+**TIBCO Spotfire** is the main tool used by SCP to visualize and analyze available data (e.g. results of model runs).
+
+**Runs Controller** is a system that allows for execution of model runs. It distributes model runs to available resources and keeps track of executed, executing and queued model runs.
+
+**Assumption Management System (AMS)** allows user to configure model runs:
+*select model version
+*define assumptions that are used as model inputs
+*configure other model run parameters
+
+**Model Outputs Storage** stores results of model runs so they are available for further analysis (using Spotfire or other tools)
+
+**Analytical Data Storage** in the context of this document is a collection of databases that store "raw" model inputs and other data required for questions from the business.
+
+Another two software systems - **Hangfire (AMS Service)** and **Control Framework** have a similar roles of executing background tasks - either recurring or on demand. Historically, Control Framework was created first and implemented as a set of SSIS (SQL Server Integration Services) packages, because most of the identified background tasks were very data (and database) centric. Hangfire (AMS Service) was created later to support tasks that were not easily implementable in SSIS.
+
+It is worth noting that some **users outside of SCP** have (or will) have and access to some dashboards in SCP Spotfire and that large part of data managed by **Analytical Data Store** are imported from **systems external to SCP**.
+
+The rest of this document describes AMS, Hangfire and Runs Controller in greater details.
+
+## AMS and Hangfire System Context
+
+AMS and Hangfire could really be considered as a single software system. Hangfire deployment unit (windows service )is even called "AMS Service".
+
+Indeed, many of background tasks are just "offloaded" by AMS to Hangfire.
+However, there are also some tasks (e.g. model outputs registration and retention policy) that are really not that much related to AMS - hence the separation to two software systems.
+
+Although for the purpose of this document they are formally separated, they also share some software components and most of their code base and that is why they are documented in same sections.
+
+AMS is the main system for storing and managing assumptions which are used as inputs for model runs.
+It also allows user to manage model versions and specify model run parameters.
+
+Hangfire (AMS Service) ...
+
+AMS creates submissions in Runs Controller - submission being a set of model runs with same inputs and model version with the only difference being a seed number used for random number generators.
+
+![AMS system context diagram](embed:AMSSystemContext)
+
+## Runs Controller System Context
+
+![Runs Controller system context diagram](embed:RunsControllerSystemContext)
+
+
--- structurizr/Program.cs
+++ structurizr/Program.cs
@@ -5,6 +5,7 @@ using OpenSoftware.Structurizr.Dgml;
 using Structurizr;
 using Structurizr.Api;
 using Structurizr.Documentation;
+using Structurizr.IO.PlantUML;
 
 namespace structurizr
 {
@@ -26,40 +27,92 @@ namespace structurizr
             // a Structurizr workspace is the wrapper for a software architecture model, views and documentation
             Workspace workspace = new Workspace("Supply Chain Planning", "Model of software systems used by Supply Chain Planning.");
 
+            #region Model
 
             Model model = workspace.Model;
 
             #region Users
-            Person amsUser = model.AddPerson("AMS User", "Modeller, Data Analyst or other users.");
+
+            Person scpUser = model.AddPerson(Location.Internal, "SCP User", "Modeller, Data Analyst or other users.");
+            Person externalUser = model.AddPerson(Location.External, "External User", "Other users outside of SCP");
+
             #endregion
 
             #region Software systems
 
-            SoftwareSystem amsSoftwareSystem = model.AddSoftwareSystem("AMS", "Assumption Management System\n\n" +
-            "Stores and allows user to manage model versions and model inputs (assumptions)");
+            SoftwareSystem amsSoftwareSystem = model.AddSoftwareSystem(
+                Location.Internal,
+                "AMS",
+                "Assumption Management System\n\nStores and allows user to manage model versions and model inputs (assumptions)");
 
-            SoftwareSystem runsControllerSoftwareSystem = model.AddSoftwareSystem("Runs Controller", "Executes model submissions and post-processing tasks and allows user to monitor and manage submisions");
-            amsUser.Uses(runsControllerSoftwareSystem, "Monitors and manages submissions");
+            SoftwareSystem runsControllerSoftwareSystem = model.AddSoftwareSystem(
+                Location.Internal,
+                "Runs Controller",
+                "Executes model submissions and post-processing tasks and allows user to monitor and manage submissions");
 
-            SoftwareSystem modelOutputsStorageSoftwareSystem = model.AddSoftwareSystem("Model Outputs Storage", "Databases and shared file system that stores outputs of model runs");
+            SoftwareSystem modelOutputsStorageSoftwareSystem = model.AddSoftwareSystem(
+                Location.Internal,
+                "Model Outputs Storage",
+                "Databases and shared file system that stores outputs of model runs");
 
-            SoftwareSystem hangfireSoftwareSystem = model.AddSoftwareSystem("Hangfire", "Recurring and background jobs processing\n\n" +
-            "Executes long-running background tasks (like submission creation in Runs Controller) " +
-            "and recurring tasks (like model outputs registration and retention policy)");
+            SoftwareSystem hangfireSoftwareSystem = model.AddSoftwareSystem(
+                Location.Internal,
+                "Hangfire",
+                "Recurring and background jobs processing\n\n" +
+                "Executes long-running background tasks (like submission creation in Runs Controller) " +
+                "and recurring tasks (like model outputs registration and retention policy)");
+
+            SoftwareSystem analyticalDataStoreSoftwareSystem = model.AddSoftwareSystem(
+                Location.Internal, 
+                "Analytical Data Store", 
+                "This includes all databases and other data stores that can be used as data sources for Spotfire or AMS");
+
+            SoftwareSystem spotfireSoftwareSystem = model.AddSoftwareSystem(
+                Location.Internal, 
+                "Spotfire", 
+                "TIBCO Spotfire dashboards visualize data (model outputs and other available data)");
+
+            SoftwareSystem externalDataSourcesSoftwareSystem = model.AddSoftwareSystem(
+                Location.External, 
+                "External Data Source", 
+                "All other source of data external to SCP from which import data");
+
+            SoftwareSystem controlFrameworkSoftwareSystem = model.AddSoftwareSystem(
+                Location.Internal, 
+                "Control Framework", 
+                "Collection of configurable SSIS packages running as SQL jobs perfroming data-centric background and reccuring tasks");
 
             #endregion
 
             #region Relationships (what uses what)
 
-            amsUser.Uses(amsSoftwareSystem, "Manages model versions, model inputs and creates submissions");
+            scpUser.Uses(amsSoftwareSystem, "Manages model versions, model inputs and creates submissions");
+            scpUser.Uses(runsControllerSoftwareSystem, "Monitors and manages submissions");
+            scpUser.Uses(spotfireSoftwareSystem, "Uses dashboards to visualize and analyze data");
+
+            externalUser.Uses(spotfireSoftwareSystem, "Uses public dashboards or outputs produced by project teams using Spotfire");
 
             amsSoftwareSystem.Uses(runsControllerSoftwareSystem, "Fetches submission status");
             amsSoftwareSystem.Uses(hangfireSoftwareSystem, "Triggers submission creation in Runs Controller");
+            amsSoftwareSystem.Uses(analyticalDataStoreSoftwareSystem, "Imports data from analytical data store that are used as model inputs (e.g. actual supply chain data)");
+            amsSoftwareSystem.Uses(controlFrameworkSoftwareSystem, "Provides user interface for Control Framework configuration; references Control Framework messages as model inputs");
+
+            controlFrameworkSoftwareSystem.Uses(analyticalDataStoreSoftwareSystem, "Imports (into) and transforms data");
+            controlFrameworkSoftwareSystem.Uses(externalDataSourcesSoftwareSystem, "Imports data (from)");
 
             runsControllerSoftwareSystem.Uses(modelOutputsStorageSoftwareSystem, "Generates model outputs");
 
             hangfireSoftwareSystem.Uses(modelOutputsStorageSoftwareSystem, "Registers created model outputs, applies retention policy");
             hangfireSoftwareSystem.Uses(runsControllerSoftwareSystem, "Creates model submissions and post-processing submissions");
+
+            analyticalDataStoreSoftwareSystem.Uses(amsSoftwareSystem, "Has read-only access to AMS data (project hierarchy, object model, reference data and objects)");
+            analyticalDataStoreSoftwareSystem.Uses(externalDataSourcesSoftwareSystem, "Imports data from external systems.");
+
+            spotfireSoftwareSystem.Uses(modelOutputsStorageSoftwareSystem, "Visualizes model outputs");
+            spotfireSoftwareSystem.Uses(analyticalDataStoreSoftwareSystem, "Visualizes other available data");
+            spotfireSoftwareSystem.Uses(runsControllerSoftwareSystem, "Reads submission details for environment performance dashboard");
+
+            #endregion
 
             #endregion
 
@@ -68,16 +121,49 @@ namespace structurizr
             // define some views (the diagrams you would like to see)
             ViewSet views = workspace.Views;
 
-            SystemContextView amsSystemContextView = views.CreateSystemContextView(amsSoftwareSystem, "AMSSystemContext",
+            #region Enterprise context
+
+            EnterpriseContextView enterpriseContextView = views.CreateEnterpriseContextView("EnterpriseContext", "Enterprise Context diagram.");
+
+            enterpriseContextView.PaperSize = PaperSize.A4_Landscape;
+            enterpriseContextView.AddAllSoftwareSystems();
+            enterpriseContextView.AddAllPeople();
+
+            #endregion
+
+            #region AMS System Context
+
+            SystemContextView amsSystemContextView = views.CreateSystemContextView(
+                amsSoftwareSystem, 
+                "AMSSystemContext",
                 "AMS System Context diagram.");
-            amsSystemContextView.PaperSize = PaperSize.A4_Landscape;
-            amsSystemContextView.AddAllSoftwareSystems();
-            amsSystemContextView.AddAllPeople();
+            // amsSystemContextView.CopyLayoutInformationFrom(enterpriseContextView);
+            // amsSystemContextView.PaperSize = PaperSize.A4_Landscape;
+            amsSystemContextView.AddNearestNeighbours(amsSoftwareSystem);
+            amsSystemContextView.AddNearestNeighbours(hangfireSoftwareSystem);
+           
+            #endregion
+
+            #region Runs Controller System Context
+
+            SystemContextView runsControllerSystemContextView = views.CreateSystemContextView(
+                runsControllerSoftwareSystem, 
+                "RunsControllerSystemContext",
+                "RunsController System Context diagram.");
+            runsControllerSystemContextView.AddNearestNeighbours(runsControllerSoftwareSystem);
+            // runsControllerSystemContextView.CopyLayoutInformationFrom(enterpriseContextView);
+           
+            #endregion
+
+            #region Styles
 
             // add some styling
             Styles styles = views.Configuration.Styles;
             styles.Add(new ElementStyle(Tags.SoftwareSystem) { Background = "#1168bd", Color = "#ffffff" });
             styles.Add(new ElementStyle(Tags.Person) { Background = "#08427b", Color = "#ffffff", Shape = Shape.Person });
+            // styles.Add(new RelationshipStyle(Tags.Relationship) {Routing = Routing.Orthogonal, Position = 30 });
+
+            #endregion
 
             #endregion
 
@@ -87,21 +173,36 @@ namespace structurizr
             StructurizrDocumentationTemplate template = new StructurizrDocumentationTemplate(workspace);
 
             var documentationFolderPath = Path.Combine(AppContext.BaseDirectory, "Documentation");
-            var amsDocPath = Path.Combine(documentationFolderPath, "AMSSoftwareSystem.md");
+
+            var amsDocPath = Path.Combine(documentationFolderPath, "Context.md");
             var amsDocFi = new FileInfo(amsDocPath);
 
             template.AddContextSection(amsSoftwareSystem, amsDocFi);
 
+            var rcDocPath = Path.Combine(documentationFolderPath, "RunsControllerContext.md");
+            var rcDocFi = new FileInfo(rcDocPath);
+
+            // template.AddContextSection(runsControllerSoftwareSystem, rcDocFi);
+
+            
             #endregion
 
             #region Upload and generate local DGML
 
+            // upload workspace to Structurizr (https://structurizr.com/)
             UploadWorkspaceToStructurizr(workspace);
 
-            // Convert to DGML
+            // Convert diagrams to DGML - dgml files can be opened using Visual Studio (extension is needed for VS 2017)
             var dgml = workspace.ToDgml();
-            // Write to file
             dgml.WriteToFile("c4model.dgml");
+
+            // Convert diagrams to PlantUML format (http://plantuml.com/)
+            StringWriter stringWriter = new StringWriter();
+            PlantUMLWriter plantUMLWriter = new PlantUMLWriter();
+            plantUMLWriter.Write(workspace, stringWriter);
+            // content of the generated file can be visualized online (http://www.plantuml.com/plantuml/uml/)
+            // or converted to image locally (using local PlantUML jar + Graphwiz or one of available VS Code extensions)
+            File.WriteAllText("c4model_plant_UML.txt", stringWriter.ToString());
 
             #endregion
         }
